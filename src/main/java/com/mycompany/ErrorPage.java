44b82f07472d07b4c1bc991c11c619a4dd9ddac7
Shawn McKinney <smckinney@apache.org>
2015-03-12 17:16:13 -0500

    remove link from error page

--- src/main/java/com/mycompany/ErrorPage.java
+++ src/main/java/com/mycompany/ErrorPage.java
@@ -18,6 +18,5 @@ public class ErrorPage extends MyBasePage
     {
         add( new Label( "title", new Model<>( "Runtime Exception Occurred" ) ) );
         add( new Label( "message", new Model<>( e.getLocalizedMessage() ) ) );
-        add( new BookmarkablePageLink( "homePage", getApplication().getHomePage() ) );
     }
 }
